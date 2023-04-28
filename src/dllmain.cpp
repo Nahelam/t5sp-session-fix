544c7eeae85af3ae0954878089b7d281a28ba9ac
Nehalem <128867759+Nahelam@users.noreply.github.com>
2023-04-28 20:58:35 +0200

    Oops

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -4,7 +4,7 @@
 
 BOOL APIENTRY DllMain(HMODULE /*module_*/, DWORD ul_reason_for_call, LPVOID /*reserved_*/)
 {
-    if (game::environment::is_sp)
+    if (game::environment::is_sp())
     {
         if (ul_reason_for_call == DLL_PROCESS_ATTACH)
         {
