0de3642809bd2d7284a40f66c2dc9579bb7735d9
Nehalem <128867759+Nahelam@users.noreply.github.com>
2023-04-28 20:52:45 +0200

    Space gang

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -3,20 +3,20 @@
 
 namespace game
 {
-	gamemode current = *reinterpret_cast<int*>(0x401337) == 0x281488C0
-		? gamemode::multiplayer
-		: gamemode::singleplayer;
+    gamemode current = *reinterpret_cast<int*>(0x401337) == 0x281488C0
+        ? gamemode::multiplayer
+        : gamemode::singleplayer;
 
-	namespace environment
-	{
-		bool is_mp()
-		{
-			return current == gamemode::multiplayer;
-		}
+    namespace environment
+    {
+        bool is_mp()
+        {
+            return current == gamemode::multiplayer;
+        }
 
-		bool is_sp()
-		{
-			return current == gamemode::singleplayer;
-		}
-	}
+        bool is_sp()
+        {
+            return current == gamemode::singleplayer;
+        }
+    }
 }
